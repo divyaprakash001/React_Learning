4d1f2c0e33eeb80cd770cea0498f328dccadd6c5
Divya Prakash <divyaprakashara09@gmail.com>
2025-01-20 14:02:20 +0530

    megaBlog project

---
+++ 12MegaBlogAppwrite/src/App.jsx
@@ -0,0 +1,14 @@
+import conf from "./conf/conf";
+
+function App() {
+  console.log(conf.appwriteUrl);
+  
+
+  return (
+    <>
+      <h1>Blog App With AppWrite</h1>
+    </>
+  )
+}
+
+export default App
